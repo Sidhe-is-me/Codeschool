36c029a84ec842452fa84c79612bef849a0ce185
Sidhe-is-me <yvonnedreptate@gmail.com>
2017-01-10 19:47:08 -0500

    Homework
    Try Python homework

---
+++ montypython.py
@@ -0,0 +1,34 @@
+print "hello"
+#describe the sketch comedy group
+
+name= 'Monty Python'
+description = 'sketch comedy group '
+year = 1969
+
+sentence = name + ' is a ' + description + 'formed in ' + str(year)
+print sentence
+
+# describe Monty Pythons work
+famous_sketch1 = "\n\tHell's Grannies"
+famous_sketch2 = "\n\tThe Dead Parrot"
+print famous_sketch1
+print famous_sketch2
+
+greeting = "G'day" + " mate"
+print(greeting) 
+name = "PeeWee"
+description ="Comedian"
+year = 1990
+sentence = name + description + str(year)
+print(sentence)
+
+
+movie1 = "\n\tPrincess Bride"
+movie2 = "\n\tStar Wars"
+print 'My Favorite Movies:' , movie1 , movie2
+
+word = 'Python'
+first = word[0]
+rest= word[1:]
+result = rest + "-" + first  + "y"
+print(result)
